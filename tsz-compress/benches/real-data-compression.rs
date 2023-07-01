f8c5b7064b40128691594b9b42461fe2b04c4a0a
Yifan Zhang <yifanzhang2024@u.northwestern.edu>
2023-07-01 01:40:56 -0500

    compiled successfully

--- tsz-compress/benches/real-data-compression.rs
+++ tsz-compress/benches/real-data-compression.rs
@@ -1,64 +1,74 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use tokio::io::{AsyncReadExt, BufReader};
 use tokio::fs::File;
+use tokio::runtime::Runtime;
 use async_compression::tokio::bufread::ZstdDecoder;
-use std::convert::TryInto;
-use log::{error, info, trace};
+use core::convert::TryInto;
+use log::{error, info};
 use tokio::task::spawn_blocking;
-use tsz_compress::prelude::{BitBuffer, BitBufferSlice, Decompressor};
+use tsz_compress::prelude::*;
 
 const BATCH_SIZE: usize = 100_000;
 
-//TODO: Debug and write structs for keys and values
+#[derive(Copy, Clone, DeltaEncodable, Compressible, Decompressible)]
+struct MyKeyStruct {
+    timestamp_us: i64,
+    partition: i64,
+}
 
-async fn decompress_data(batch_size: usize) -> Result<(), Box<dyn std::error::Error>> {
-    let file_path = "tests/data/0001-1686168000000000-9223372037005771051-index.bin";
-    let file = File::open(file_path).await?;
-    let bytes = file.metadata().await?.len();
-    let mut buf_reader = BufReader::new(file);
+#[derive(Copy, Clone, DeltaEncodable, Compressible, Decompressible)]
+struct MyValueStruct {
+    value: i64,
+}
+
+async fn decompress_data(batch_size: usize) -> Result<(), String> {
+    let file_path: &str = "tests/data/0001-1686168000000000-9223372037005771051-index.bin";
+    let file = File::open(file_path).await.map_err(|err| err.to_string())?;
+    let bytes = file.metadata().await.map_err(|err| err.to_string())?.len();
+    let buf_reader = BufReader::new(file);
     let mut zfile = ZstdDecoder::new(buf_reader);
     let mut compressed = Vec::new();
-    zfile.read_to_end(&mut compressed).await?;
+    zfile.read_to_end(&mut compressed).await.map_err(|err| err.to_string())?;
     drop(zfile);
 
-    let mut index = spawn_blocking(move || {
+    let result = spawn_blocking(move || {
         // Expect the header to be "TKSS"
         let header = "_TKSS_0001_".as_bytes();
         if &compressed[0..header.len()] != header {
-            return Err(format!("Invalid Header: {}", file_path.clone().into()));
+            return Err(format!("Invalid Header: {}", <&str as Into<String>>::into(file_path.clone())));
         }
         let compressed = &compressed[header.len()..];
 
         // The next 8 bytes are the u64 byte length of the compressed data
-        let keys_byte_size = u64::from_be_bytes(compressed[..8].try_into()?) as usize;
-        if keys_byte_size + 16 > compressed.len() {
+        let keys_byte_size = u64::from_be_bytes(compressed[..8].try_into().map_err(|_| "Invalid length".to_string())?);
+        if keys_byte_size + 16 > compressed.len() as u64 {
             error!(
                 "Not enough bytes left: {} < {}",
                 compressed.len(),
-                (keys_byte_size + 16)
+                keys_byte_size + 16
             );
-            return Err(format!("Invalid Length: {}", file_path.clone().into()));
+            return Err(format!("Invalid Length: {}", <&str as Into<String>>::into(file_path.clone())));
         }
         let compressed = &compressed[8..];
 
         // The next 8 bytes are the u64 bit length of the compressed data
-        let keys_bit_size = u64::from_be_bytes(compressed[..8].try_into()?) as usize;
+        let keys_bit_size = u64::from_be_bytes(compressed[..8].try_into().map_err(|_| "Invalid length".to_string())?);
         if keys_bit_size as usize > compressed.len() * 8 {
             error!(
                 "More bits than bytes: {} > {}",
                 keys_bit_size,
                 compressed.len() * 8
             );
-            return Err(format!("Invalid Length: {}", file_path.clone().into()));
+            return Err(format!("Invalid Length: {}", <&str as Into<String>>::into(file_path.clone())));
         }
         // Confirm there are keys_byte_size + 16 bytes left at minimum
-        if keys_byte_size + 16 > compressed.len() {
+        if keys_byte_size + 16 > compressed.len() as u64 {
             error!(
                 "Not enough bytes left: {} < {}",
                 compressed.len(),
-                (keys_byte_size + 16)
+                keys_byte_size + 16
             );
-            return Err(format!("Invalid Length: {}", file_path.clone().into()));
+            return Err(format!("Invalid Length: {}", <&str as Into<String>>::into(file_path.clone())));
         }
         let compressed = &compressed[8..];
 
@@ -77,35 +87,32 @@ async fn decompress_data(batch_size: usize) -> Result<(), Box<dyn std::error::Er
         // Expect the header to be "TKSS"
         let header = "_TKSS_0001_".as_bytes();
         if &compressed[0..header.len()] != header {
-            return Err(format!("Invalid Header: {}", file_path.clone().into()));
+            return Err(format!("Invalid Header: {}", <&str as Into<String>>::into(file_path.clone())));
         }
         let compressed = &compressed[header.len()..];
 
         // The next 8 bytes are the u64 byte length of the compressed data
-        let values_byte_size = match u64::from_be_bytes(compressed[..8].try_into()) {
-            Ok(size) => size as usize,
-            Err(error) => return Err(format!("Error converting byte size: {}", error)),
-        };
-        if values_byte_size + 8 > compressed.len() {
+        let values_byte_size = u64::from_be_bytes(compressed[..8].try_into().map_err(|_| "Invalid length".to_string())?);
+        if values_byte_size + 8 > compressed.len() as u64 {
             error!(
                 "Not enough bytes left: {} < {}",
                 compressed.len(),
-                (values_byte_size + 8)
+                values_byte_size + 8
             );
-            return Err(format!("Invalid Length: {}", file_path.clone().into()));
+            return Err(format!("Invalid Length: {}", <&str as Into<String>>::into(file_path.clone())));
         }
         let compressed = &compressed[8..];
 
-        let values_bit_size = u64::from_be_bytes(compressed[..8].try_into()?) as usize;
+        let values_bit_size = u64::from_be_bytes(compressed[..8].try_into().map_err(|_| "Invalid length".to_string())?);
         let compressed = &compressed[8..];
 
-        if values_byte_size != compressed.len() {
+        if values_byte_size != compressed.len() as u64 {
             error!(
                 "Values byte size doesn't match: {} != {}",
                 values_byte_size,
                 compressed.len()
             );
-            return Err(format!("Invalid Length: {}", file_path.clone().into()));
+            return Err(format!("Invalid Length: {}", <&str as Into<String>>::into(file_path.clone())));
         }
 
         info!(
@@ -123,41 +130,47 @@ async fn decompress_data(batch_size: usize) -> Result<(), Box<dyn std::error::Er
             "Decompressing {} bytes of keys and {} bytes of values",
             keys_byte_size, values_byte_size
         );
+
+        let chunk_size = batch_size;
+        let mut decompressed_keys = Vec::new();
+        let mut decompressed_values = Vec::new();
         let start = std::time::Instant::now();
-        let mut rows = keys_decompressor
-            .decompress::<K>()
-            .zip(values_decompressor.decompress::<V>())
-            .into_iter()
-            .map(|(k, v)| Ok((k?, v?)))
-            .collect::<Result<Vec<_>, &'static str>>()?;
+
+        loop {
+            let chunk_keys = keys_decompressor.decompress::<MyKeyStruct>();
+            let chunk_values = values_decompressor.decompress::<MyValueStruct>();
+            decompressed_keys.extend(chunk_keys);
+            decompressed_values.extend(chunk_values);
+            if decompressed_keys.len() >= chunk_size && decompressed_values.len() >= chunk_size {
+                break;
+            }
+        }
+
         info!(
-            "Decompressed {} keys and values in {:?}",
-            rows.len(),
+            "Decompressed {} keys and {} values in {:?}",
+            decompressed_keys.len(),
+            decompressed_values.len(),
             start.elapsed()
         );
 
-        let CHUNK_SIZE: usize = batch_size;
-        rows.chunks(CHUNK_SIZE)
-            .into_iter()
-            .enumerate()
-            .for_each(|(i, chunk)| {
-                let start = std::time::Instant::now();
-                index.index.insert_sorted(chunk);
-                trace!("Inserted {} in {:?}", i, start.elapsed());
-            });
-
-        Ok(index)
+        Ok(())
     })
-    .await??;
+    .await;
 
-    Ok(())
+    result.unwrap_or_else(|err| {
+        error!("Decompression error: {}", err);
+        Err(err.to_string())
+    })
 }
 
 fn benchmark_decompress_data(c: &mut Criterion) {
     c.bench_function("decompress_data_batch_100000", |b| {
-        b.iter(|| block_on(decompress_data_batch(BATCH_SIZE)))
+        b.iter(|| {
+            let rt = Runtime::new().unwrap();
+            rt.block_on(decompress_data(BATCH_SIZE))
+        })
     });
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group!(benches, benchmark_decompress_data);
 criterion_main!(benches);
