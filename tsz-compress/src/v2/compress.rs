bbf482968d0148fd80b48e3867ed6b1949bf60c7
Jacob Trueb <jtrueb@northwestern.edu>
2023-10-11 16:25:40 -0500

    WIP: Changing V2 Compressor interface

---
+++ tsz-compress/src/v2/compress.rs
@@ -0,0 +1,205 @@
+use crate::prelude::BitBuffer;
+use alloc::boxed::Box;
+use alloc::vec::Vec;
+use core::fmt::Debug;
+
+use super::greedy_delta_v2::Queue;
+
+///
+/// Data comes in one row at a time, living for the duration of the function call
+///
+/// A compressor will make a copy of each column value into a column queue
+///
+/// A compressor will iterate over each column queue to decide to emit compressed bits
+///
+/// A column queue will pop N values at a time and a column bit buffer will be extended
+///
+
+pub struct Compressor<T: CompressibleRow> {
+    columns: Vec<Box<dyn ColumnCompressor>>,
+    _phantom: core::marker::PhantomData<T>,
+}
+
+pub trait CompressibleRow {
+    const NUM_COLUMNS: usize;
+
+    fn init_compressors() -> Vec<Box<dyn ColumnCompressor>>;
+    fn as_ptr(&self, index: usize) -> *const u8;
+}
+
+pub trait ColumnCompressor {
+    /// Consume a value and maybe emit bits
+    fn push(&mut self, value: *const u8);
+}
+
+pub struct ColumnCompressorImpl<T> {
+    queue: Queue<T, 10>,
+    output: BitBuffer,
+}
+
+impl<T: Debug + Copy + ZigZagBits> ColumnCompressor for ColumnCompressorImpl<T> {
+    fn push(&mut self, value: *const u8) {
+        //
+        // Safety: We know that the value is valid for the lifetime of the row
+        //        and that it is a valid T.
+        //       We also know that the queue is not full.
+        //
+        self.queue.push(unsafe { *(value as *const T) });
+
+        //
+        // If the queue is full, we need to decide how many samples to pop
+        //
+        if self.queue.is_full() {
+            for t in self.queue.data.iter() {
+                #[cfg(test)]
+                {
+                    println!(
+                        "{:?} requires {} or {} bits",
+                        t,
+                        t.delta_bits(),
+                        t.delta_delta_bits()
+                    )
+                }
+            }
+            self.queue.pop();
+        }
+    }
+}
+
+impl<T> ColumnCompressorImpl<T> {
+    fn new() -> Self {
+        ColumnCompressorImpl {
+            queue: Queue::new(),
+            output: BitBuffer::new(),
+        }
+    }
+}
+
+impl<T: CompressibleRow> Compressor<T> {
+    pub fn new() -> Self {
+        Compressor {
+            columns: T::init_compressors(),
+            _phantom: core::marker::PhantomData,
+        }
+    }
+
+    pub fn compress(&mut self, row: T) {
+        for i in 0..T::NUM_COLUMNS {
+            // Push a copy into the column queue
+            self.columns[i].push(row.as_ptr(i));
+        }
+    }
+
+    pub fn finish(self) -> BitBuffer {
+        todo!();
+    }
+}
+
+trait ZigZagBits {
+    fn delta_bits(&self) -> usize;
+    fn delta_delta_bits(&self) -> usize;
+}
+
+impl ZigZagBits for i8 {
+    fn delta_bits(&self) -> usize {
+        // todo check number of bits needed based on magnitude and cutoffs
+        8
+    }
+
+    fn delta_delta_bits(&self) -> usize {
+        // todo check number of bits needed based on magnitude and cutoffs
+        90
+    }
+}
+
+impl ZigZagBits for i16 {
+    fn delta_bits(&self) -> usize {
+        // todo check number of bits needed based on magnitude and cutoffs
+        16
+    }
+
+    fn delta_delta_bits(&self) -> usize {
+        // todo check number of bits needed based on magnitude and cutoffs
+        91
+    }
+}
+
+impl ZigZagBits for i32 {
+    fn delta_bits(&self) -> usize {
+        // todo check number of bits needed based on magnitude and cutoffs
+        32
+    }
+
+    fn delta_delta_bits(&self) -> usize {
+        // todo check number of bits needed based on magnitude and cutoffs
+        92
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    pub struct TestRow {
+        pub a: i8,
+        pub b: i16,
+        pub c: i32,
+    }
+
+    impl CompressibleRow for TestRow {
+        const NUM_COLUMNS: usize = 3;
+
+        fn init_compressors() -> Vec<Box<dyn ColumnCompressor>> {
+            vec![
+                Box::new(ColumnCompressorImpl::<i8>::new()),
+                Box::new(ColumnCompressorImpl::<i16>::new()),
+                Box::new(ColumnCompressorImpl::<i32>::new()),
+            ]
+        }
+
+        fn as_ptr(&self, index: usize) -> *const u8 {
+            match index {
+                0 => &self.a as *const i8 as *const u8,
+                1 => &self.b as *const i16 as *const u8,
+                2 => &self.c as *const i32 as *const u8,
+                _ => panic!("Invalid index"),
+            }
+        }
+    }
+
+    #[test]
+    fn can_init() {
+        let mut compressor = Compressor::<TestRow>::new();
+    }
+
+    #[test]
+    fn can_push_row() {
+        let mut compressor = Compressor::<TestRow>::new();
+        let row = TestRow { a: 1, b: 2, c: 3 };
+        compressor.compress(row);
+        // todo check exact bits and rows match
+    }
+
+    #[test]
+    fn can_finish() {
+        let mut compressor = Compressor::<TestRow>::new();
+        let row = TestRow { a: 1, b: 2, c: 3 };
+        compressor.compress(row);
+        let _bits = compressor.finish();
+        // todo check exact bits and rows match
+    }
+
+    #[test]
+    fn can_compress_11() {
+        let mut compressor = Compressor::<TestRow>::new();
+        for i in 0..11 {
+            let row = TestRow {
+                a: i,
+                b: i as i16 * 10,
+                c: i as i32 * 100,
+            };
+            compressor.compress(row);
+        }
+        let _bits = compressor.finish();
+    }
+}
--- tsz-compress/src/v2/greedy_delta_v2.rs
+++ tsz-compress/src/v2/greedy_delta_v2.rs
@@ -2,23 +2,19 @@ extern crate std;
 
 use std::collections::VecDeque;
 
-const QUEUE_CAPACITY: usize = 10;
-
-pub struct Queue<T> {
-    data: VecDeque<T>,
-    capacity: usize,
+pub struct Queue<T, const N: usize> {
+    pub(crate) data: VecDeque<T>,
 }
 
-impl<T> Queue<T> {
+impl<T, const N: usize> Queue<T, N> {
     pub fn new() -> Self {
         Queue {
-            data: VecDeque::with_capacity(10),
-            capacity: QUEUE_CAPACITY,
+            data: VecDeque::with_capacity(N),
         }
     }
 
     pub fn is_full(&mut self) -> bool {
-        if (self.data.len() == self.capacity) {
+        if self.data.len() >= N {
             return true;
         }
         return false;
@@ -30,5 +26,19 @@ impl<T> Queue<T> {
 
     pub fn pop(&mut self) -> Option<T> {
         return self.data.pop_front();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        let mut queue: Queue<usize, 10> = Queue::new();
+        let pushed_value = 8;
+        queue.push(pushed_value);
+        let popped_value = queue.pop().unwrap();
+        assert_eq!(pushed_value, popped_value);
     }
 }
