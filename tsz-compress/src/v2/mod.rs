9aac6e0a38d28c0bacb2184afd0a17d534f7f327
Jacob Trueb <jtrueb@northwestern.edu>
2023-10-12 14:14:34 -0500

    Add CompressV2 proc macro (#12)
    * Add integration test expansion of CompressV2 proc macro
    
    * Add TszCompressV2 trait with proc macro text

--- tsz-compress/src/v2/mod.rs
+++ tsz-compress/src/v2/mod.rs
@@ -1,1 +1,84 @@
+use crate::prelude::*;
+
 pub mod queue;
+pub use queue::*;
+
+///
+/// Pop N values from the queue and emit them to the bit buffer.
+/// N may be 0 if flush is false.
+///
+/// Returns the number of elements popped from the queue.
+///
+#[allow(unused)]
+pub fn emit_bits<T>(q: &mut CompressionQueue<T, 10>, out: &mut BitBuffer, flush: bool) -> usize {
+    // todo
+    0
+}
+
+///
+/// High-level interface for compression.
+///
+pub trait TszCompressV2 {
+    /// The type of the row to compress.
+    type T: Copy;
+
+    ///
+    /// Lazily compress a row.
+    ///
+    fn compress(&mut self, row: Self::T);
+
+    ///
+    /// The number of bits that have been compressed.
+    /// This is an estimate, as the last few samples may have been emitted are estimated.
+    ///
+    fn len(&self) -> usize;
+
+    ///
+    /// Return an estimate of bits per column value as the number of
+    /// compressed bits / count of column values compressed / columns per row.
+    ///
+    fn bit_rate(&self) -> usize;
+
+    ///
+    /// Finish compression and return the compressed data.
+    ///
+    fn finish(self) -> BitBuffer;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_impl_compress() {
+        #[derive(Copy, Clone)]
+        struct TestRow {
+            a: i32,
+            b: i64,
+        }
+        struct CompressorImpl {
+            a_queue: CompressionQueue<i32, 10>,
+            b_queue: CompressionQueue<i64, 10>,
+        }
+
+        impl TszCompressV2 for CompressorImpl {
+            type T = TestRow;
+            fn compress(&mut self, row: TestRow) {
+                self.a_queue.push(row.a);
+                self.b_queue.push(row.b);
+            }
+
+            fn len(&self) -> usize {
+                self.a_queue.len() + self.b_queue.len()
+            }
+
+            fn bit_rate(&self) -> usize {
+                0
+            }
+
+            fn finish(self) -> BitBuffer {
+                BitBuffer::new()
+            }
+        }
+    }
+}
---
+++ tsz-compress/tests/compress-v2.rs
@@ -0,0 +1,19 @@
+#![allow(unused)]
+use tsz_compress::prelude::*;
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn can_expand_v2() {
+        #[derive(Copy, Clone, CompressV2)]
+        struct TestRow {
+            a: i8,
+            b: i16,
+            c: i32,
+            d: i64,
+        }
+    }
+}
