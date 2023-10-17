bbaa206fa6e7373c873620b0f726781a38b68ee7
Amey Kasbe <amey.kasbe@gmail.com>
2023-10-17 16:52:25 -0500

    Update sanity tests: Test remaining queue length

--- tsz-compress/src/v2/mod.rs
+++ tsz-compress/src/v2/mod.rs
@@ -546,6 +546,7 @@ mod tests {
         let decoded_values = decode_delta(&bits).unwrap();
         assert_eq!(values[..2], decoded_values[..2]);
         assert_eq!(num_emitted_samples, 2);
+        assert_eq!(queue.len(), 8);
     }
     #[test]
     fn can_encode_decode_case2() {
@@ -560,6 +561,7 @@ mod tests {
         let decoded_values = decode_delta(&bits).unwrap();
         assert_eq!(values[..3], decoded_values[..3]);
         assert_eq!(num_emitted_samples, 3);
+        assert_eq!(queue.len(), 7);
     }
 
     #[test]
@@ -576,6 +578,7 @@ mod tests {
         let decoded_values = decode_delta(&bits).unwrap();
         assert_eq!(values[..4], decoded_values[..4]);
         assert_eq!(num_emitted_samples, 4);
+        assert_eq!(queue.len(), 6);
     }
 
     #[test]
@@ -592,6 +595,7 @@ mod tests {
         let decoded_values = decode_delta(&bits).unwrap();
         assert_eq!(values[..8], decoded_values[..8]);
         assert_eq!(num_emitted_samples, 8);
+        assert_eq!(queue.len(), 2);
     }
 
     #[test]
@@ -606,7 +610,8 @@ mod tests {
         let num_emitted_samples = queue.emit_bits(&mut bits, true);
         let decoded_values = decode_delta(&bits).unwrap();
         assert_eq!(values, decoded_values);
-        assert_eq!(num_emitted_samples, 10)
+        assert_eq!(num_emitted_samples, 10);
+        assert_eq!(queue.len(), 0);
     }
 
     #[test]
