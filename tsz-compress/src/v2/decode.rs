36bfd8a95d050dadf86f9d744c1ad6ec4c8f808f
Amey Kasbe <40777379+ameykasbe@users.noreply.github.com>
2023-11-21 16:46:34 -0600

    Add seperators in binary representations (#22)

--- tsz-compress/src/v2/decode.rs
+++ tsz-compress/src/v2/decode.rs
@@ -160,7 +160,7 @@ pub fn decode_i8<'it>(iter: &mut HalfIter<'it>, output: &mut Vec<i8>) -> Result<
                 let bit_width = 6;
                 let shift = 32 - padding - bit_width;
                 for i in 0..5 {
-                    let delta = ((word >> (shift - bit_width * i)) & 0b111111) as i8;
+                    let delta = ((word >> (shift - bit_width * i)) & 0b11_1111) as i8;
                     let delta = (delta >> 1) ^ -(delta & 1);
                     value = (value as i16 + delta as i16) as i8;
                     output.push(value);
@@ -336,7 +336,7 @@ pub fn decode_i16<'it>(iter: &mut HalfIter<'it>, output: &mut Vec<i16>) -> Resul
                 let bit_width = 6;
                 let shift = 32 - padding - bit_width;
                 for i in 0..5 {
-                    let delta = ((word >> (shift - bit_width * i)) & 0b111111) as i16;
+                    let delta = ((word >> (shift - bit_width * i)) & 0b11_1111) as i16;
                     let delta = (delta >> 1) ^ -(delta & 1);
                     value = (value as i32 + delta as i32) as i16;
                     output.push(value);
@@ -378,7 +378,7 @@ pub fn decode_i16<'it>(iter: &mut HalfIter<'it>, output: &mut Vec<i16>) -> Resul
                 let bit_width = 10;
                 let shift = 32 - padding - bit_width;
                 for i in 0..3 {
-                    let delta = ((word >> (shift - bit_width * i)) & 0b1111111111) as i16;
+                    let delta = ((word >> (shift - bit_width * i)) & 0b11_1111_1111) as i16;
                     let delta = (delta >> 1) ^ -(delta & 1);
                     value = (value as i32 + delta as i32) as i16;
                     output.push(value);
@@ -524,7 +524,7 @@ pub fn decode_i32<'it>(iter: &mut HalfIter<'it>, output: &mut Vec<i32>) -> Resul
                 let bit_width = 6;
                 let shift = 32 - padding - bit_width;
                 for i in 0..5 {
-                    let delta = ((word >> (shift - bit_width * i)) & 0b111111) as i32;
+                    let delta = ((word >> (shift - bit_width * i)) & 0b11_1111) as i32;
                     let delta = (delta >> 1) ^ -(delta & 1);
                     value = (value as i64 + delta as i64) as i32;
                     output.push(value);
@@ -567,7 +567,7 @@ pub fn decode_i32<'it>(iter: &mut HalfIter<'it>, output: &mut Vec<i32>) -> Resul
                 let bit_width = 10;
                 let shift = 32 - padding - bit_width;
                 for i in 0..3 {
-                    let delta = ((word >> (shift - bit_width * i)) & 0b1111111111) as i32;
+                    let delta = ((word >> (shift - bit_width * i)) & 0b11_1111_1111) as i32;
                     let delta = (delta >> 1) ^ -(delta & 1);
                     value = (value as i64 + delta as i64) as i32;
                     output.push(value);
@@ -737,7 +737,7 @@ pub fn decode_i64<'it>(iter: &mut HalfIter<'it>, output: &mut Vec<i64>) -> Resul
                 let bit_width = 6;
                 let shift = 32 - padding - bit_width;
                 for i in 0..5 {
-                    let delta = ((word >> (shift - bit_width * i)) & 0b111111) as i32;
+                    let delta = ((word >> (shift - bit_width * i)) & 0b11_1111) as i32;
                     let delta = (delta >> 1) ^ -(delta & 1);
                     value = (value as i128 + delta as i128) as i64;
                     output.push(value);
@@ -780,7 +780,7 @@ pub fn decode_i64<'it>(iter: &mut HalfIter<'it>, output: &mut Vec<i64>) -> Resul
                 let bit_width = 10;
                 let shift = 32 - padding - bit_width;
                 for i in 0..3 {
-                    let delta = ((word >> (shift - bit_width * i)) & 0b1111111111) as i32;
+                    let delta = ((word >> (shift - bit_width * i)) & 0b11_1111_1111) as i32;
                     let delta = (delta >> 1) ^ -(delta & 1);
                     value = (value as i128 + delta as i128) as i64;
                     output.push(value);
