99327506e2cd75ad593a07b2d45b01c5c0c05885
Amey Kasbe <40777379+ameykasbe@users.noreply.github.com>
2023-11-29 13:34:42 -0600

    Add unit tests & related functionality for emit i32 delta (#36)
    * Remove: Delta-Delta unit test cases
    
    * Add impl PartialEq for HalfWord and HalfVec
    
    * Unit tests: Remove previous implementation tests & add emit delta i32 tests
    
    * Remove integration tests for i8 and i64 bit-widths
    
    * Code Refactor: Place PartialEq in mod tests & mod tests in halfvec.rs

--- tsz-compress/src/v2/halfvec.rs
+++ tsz-compress/src/v2/halfvec.rs
@@ -1,6 +1,8 @@
 #![allow(dead_code)]
 
+use crate::prelude::*;
 use alloc::vec::Vec;
+pub use queue::*;
 
 ///
 /// A dynamic vector of nibbles.
@@ -230,5 +232,192 @@ mod tests {
         // Now every nibble is pushed together
         let flat = HalfVec::finish([&queue].into_iter());
         assert_eq!(flat.len(), (128 + 8 * 128 + 1 + 1) / 2);
+    }
+}
+
+// Delta-Delta Tests
+#[cfg(test)]
+mod tests_emit_delta {
+
+    use crate::prelude::halfvec::{HalfVec, HalfWord};
+
+    use super::*;
+    use bitvec::bits;
+    use rand::Rng;
+
+    /// Compares two HalfVec for equality.
+    impl PartialEq for HalfVec {
+        fn eq(&self, other: &Self) -> bool {
+            self.words == other.words && self.len() == other.len()
+        }
+    }
+
+    /// Compares two HalfWord for equality.
+    impl PartialEq for HalfWord {
+        fn eq(&self, other: &Self) -> bool {
+            match (self, other) {
+                (HalfWord::Half(value1), HalfWord::Half(value2)) => value1 == value2,
+                (HalfWord::Byte(value1), HalfWord::Byte(value2)) => value1 == value2,
+                (HalfWord::Full(value1), HalfWord::Full(value2)) => value1 == value2,
+                _ => false,
+            }
+        }
+    }
+
+    // Helped function
+    fn _emit_delta_i32(values: Vec<i32>) -> HalfVec {
+        // Create queue
+        let mut queue: CompressionQueue<10> = CompressionQueue::new();
+
+        // Push values into queue
+        for value in &values {
+            queue.push(*value);
+        }
+
+        // Initialize bit buffer
+        let mut bits = HalfVec::new(8);
+
+        // Encode
+        queue.emit_delta_bits(&mut bits);
+
+        return bits;
+    }
+
+    #[test]
+    fn test_emit_delta_i32_sanity1() {
+        // Case 7: Encode 10 samples between [-4, 3] in 3 bits
+        let values = vec![-3, 2, 0, 1, 2, -3, -1, -2, -4, -3];
+
+        let encoded_halfvec = _emit_delta_i32(values);
+
+        // Initialize expected bit buffer
+        let mut expected_halfvec = HalfVec::new(8);
+
+        // Expecting 10 samples of 3 bits
+        expected_halfvec.push(HalfWord::Half(0b1111));
+
+        // Values: [-3, 2, 0, 1, 2, -3, -1, -2, -4, -3]
+        // Zigzag values: [5, 4, 0, 2, 4, 5, 1, 3, 7, 5]
+        // Binary of zigzag values: 00 101 100 000 010 100 101 001 011 111 101
+        let full_value = 0x2C0A_52FD;
+        expected_halfvec.push(HalfWord::Full(full_value));
+
+        // Assert equality
+        assert_eq!(encoded_halfvec, expected_halfvec);
+    }
+
+    #[test]
+    fn test_emit_delta_i32_sanity2() {
+        // Case 6 and 7: Encode 5 samples between [-32, 31] in 6 bits
+        let values = vec![-32, 31, 16, 1, 2];
+
+        let encoded_halfvec = _emit_delta_i32(values);
+
+        // Initialize expected bit buffer
+        let mut expected_halfvec = HalfVec::new(8);
+
+        // Expecting 10 samples of 3 bits
+        expected_halfvec.push(HalfWord::Half(0b1110));
+
+        // Values: [-32, 31, 16, 1, 2]
+        // Zigzag values: [63, 62, 32, 2, 4]
+        // Binary of zigzag values: 111111 111110 100000 000010 000100
+        let full_value = 0x3FFA_0084;
+        expected_halfvec.push(HalfWord::Full(full_value));
+
+        // Expected length
+        assert_eq!(encoded_halfvec, expected_halfvec);
+    }
+
+    #[test]
+    fn test_emit_delta_i32_sanity3() {
+        // Case 5, 6 and 7: Encode 4 samples between [-128, 127] in 8 bits
+        let values = vec![-128, 127, 64, 1];
+
+        let encoded_halfvec = _emit_delta_i32(values);
+
+        // Initialize expected bit buffer
+        let mut expected_halfvec = HalfVec::new(8);
+
+        // Expecting 10 samples of 3 bits
+        expected_halfvec.push(HalfWord::Half(0b1100));
+
+        // Values: [-128, 127, 64, 1]
+        // Zigzag values: [255, 254, 128, 2]
+        // Binary of zigzag values: 11111111 11111110 10000000 00000010
+        let full_value = 0xFFFE_8002;
+        expected_halfvec.push(HalfWord::Full(full_value));
+
+        // Expected length
+        assert_eq!(encoded_halfvec, expected_halfvec);
+    }
+
+    #[test]
+    fn test_emit_delta_i32_sanity4() {
+        // Case 4, 5, 6 and 7: Encode 3 samples between [-512, 511] in 10 bits
+        let values = vec![-512, 511, 256];
+
+        let encoded_halfvec = _emit_delta_i32(values);
+
+        // Initialize expected bit buffer
+        let mut expected_halfvec = HalfVec::new(8);
+
+        // Expecting 10 samples of 3 bits
+        expected_halfvec.push(HalfWord::Half(0b1010));
+
+        // Values: [-512, 511, 256]
+        // Zigzag values: [1023, 1022, 512]
+        // Binary of zigzag values: 1111111111 1111111110 1000000000
+        let full_value = 0x3FFF_FA00;
+        expected_halfvec.push(HalfWord::Full(full_value));
+
+        // Expected length
+        assert_eq!(encoded_halfvec, expected_halfvec);
+    }
+
+    #[test]
+    fn test_emit_delta_i32_sanity5() {
+        // Case 3, 4, 5, 6 and 7: Encode 2 samples between [-32768, 32767] in 16 bits
+        let values = vec![-32768, 32767];
+
+        let encoded_halfvec = _emit_delta_i32(values);
+
+        // Initialize expected bit buffer
+        let mut expected_halfvec = HalfVec::new(8);
+
+        // Expecting 10 samples of 3 bits
+        expected_halfvec.push(HalfWord::Half(0b1000));
+
+        // Values: [-32768, 32767]
+        // Zigzag values: [65535, 65534]
+        // Binary of zigzag values: 1111111111111111 1111111111111110
+        let full_value = 0xFFFF_FFFE;
+        expected_halfvec.push(HalfWord::Full(full_value));
+
+        // Expected length
+        assert_eq!(encoded_halfvec, expected_halfvec);
+    }
+
+    #[test]
+    fn test_emit_delta_i32_sanity6() {
+        // Case 2, 3, 4, 5, 6 and 7: Encode 2 samples between [-32768, 32767] in 16 bits
+        let values = vec![i32::MIN / 4, 1, 1, 1];
+
+        let encoded_halfvec = _emit_delta_i32(values);
+
+        // Initialize expected bit buffer
+        let mut expected_halfvec = HalfVec::new(8);
+
+        // Expecting 10 samples of 3 bits
+        expected_halfvec.push(HalfWord::Half(0b1011));
+
+        // Values: vec![i32::MIN / 4]
+        // Zigzag values: [-1 * i32::MIN / 2]
+        // Binary of zigzag values:
+        let full_value = 0x3FFFFFFF;
+        expected_halfvec.push(HalfWord::Full(full_value));
+
+        // Expected length
+        assert_eq!(encoded_halfvec, expected_halfvec);
     }
 }
